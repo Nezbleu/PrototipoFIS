c767de394f4523d8ae154023298bb1c61c5354c8
Nezbleu <32167661+Nezbleu@users.noreply.github.com>
2020-03-02 09:53:45 -0500

    Add files via upload

--- PrototipoFIS/src/Gestores/Elemento.java
+++ PrototipoFIS/src/Gestores/Elemento.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Gestores;
 
-/**
- *
- * @author USER
- */
 public class Elemento {
-    
+    //ATRIBUTOS
+        private int idElemento;
+        private String nombreElemento;
+        private int valorElemento;
+    //MÉTODOS
+    public int getIdElemento() {
+        return idElemento;
+    }
+
+    public void setIdElemento(int idElemento) {
+        this.idElemento = idElemento;
+    }
+
+    public String getNombreElemento() {
+        return nombreElemento;
+    }
+
+    public void setNombreElemento(String nombreElemento) {
+        this.nombreElemento = nombreElemento;
+    }
+
+    public int getValorElemento() {
+        return valorElemento;
+    }
+
+    public void setValorElemento(int valorElemento) {
+        this.valorElemento = valorElemento;
+    }
 }
--- PrototipoFIS/src/Gestores/GestorElementos.java
+++ PrototipoFIS/src/Gestores/GestorElementos.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Gestores;
 
-/**
- *
- * @author USER
- */
+import java.util.ArrayList;
+
 public class GestorElementos {
-    
+    //ATRIBUTOS
+    private ArrayList<Elemento> listaElementos;
+    //MÉTODOS
+    //INICIO GET ELEMENTO
+    public Elemento getElemento(){
+        return null;
+    }
+    public Elemento getElemento(int idElemento){
+        for (int i=0; i<listaElementos.size(); i++){
+            if(listaElementos.get(i).getIdElemento()==idElemento)
+                return listaElementos.get(i);
+        }
+        return null;
+    }
+    public Elemento getElemento(String nombreElemento){
+        for (int i=0; i<listaElementos.size(); i++){
+            if(listaElementos.get(i).getNombreElemento()==nombreElemento)
+                return listaElementos.get(i);
+        }
+        return null;
+    }
+    //FIN GET ELEMENTO
+    public void agregarElemento(String nombreElemento, int valorElemento){
+        Elemento elemento = new Elemento();
+        elemento.setIdElemento(listaElementos.size());
+        elemento.setNombreElemento(nombreElemento);
+        elemento.setValorElemento(valorElemento);
+        listaElementos.add(elemento);
+    }
+
 }
