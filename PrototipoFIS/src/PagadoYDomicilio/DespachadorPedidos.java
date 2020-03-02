37043a158c2ca28e51566c632cad4005b9605955
Nezbleu <32167661+Nezbleu@users.noreply.github.com>
2020-03-02 09:54:42 -0500

    Update DespachadorPedidos.java

--- PrototipoFIS/src/PagadoYDomicilio/DespachadorPedidos.java
+++ PrototipoFIS/src/PagadoYDomicilio/DespachadorPedidos.java
@@ -1,11 +1,21 @@
-
 package PagadoYDomicilio;
 
 import CreadorPlatillo.Platillo;
+import Gestores.GestorElementos;
+import java.util.ArrayList;
 
 public class DespachadorPedidos {
-    public Factura generarFactura(Platillo platillo){
-        return null;
+    public Factura generarFactura(Platillo platillo, GestorVentas facturador, GestorElementos elementador){
+        //Generacion de factura
+        int id = facturador.getFacturas().size();
+        Factura factura = new Factura(id);
+        //Parametro para la factura
+        ArrayList<String> elementos;
+        elementos = platillo.getIngredientes();
+        
+        //Llenado de factura
+        factura.setElementos(elementos);
+        factura.setPrecios(precios);
     }
     public Platillo obtenerPlatillo(int idPlatillo){
         return null;
@@ -14,3 +24,4 @@ public class DespachadorPedidos {
     
     }
 }
+
